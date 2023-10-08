0dd7930d486aad4902ddc6a1b7f485a163a29903
dilanelefa <lefakongdilane@gmail.com>
2023-10-08 04:03:54 +0100

    modification docx file

--- INF231/TP1/function.c
+++ INF231/TP1/function.c
@@ -8,40 +8,40 @@
 */
 
 list init(){
-  list liste = NULL;
-  return liste;
+    list liste = NULL;
+    return liste;
 }
 
 //insert the integer in the list
 void insert(list *l, int value){
-  list element = (list)malloc(sizeof(struct cellule));
-  element->next = NULL;
-  element->value = value;
+    list element = (list)malloc(sizeof(struct cellule));
+    element->next = NULL;
+    element->value = value;
 
-  if(*l == NULL){
-      *l = element;
-  }
-  else{
-    list iterator = *l;
-    while(iterator->next != NULL)
-      iterator = iterator->next;
-    iterator->next  = element;
-  }
+    if(*l == NULL){
+        *l = element;
+    }
+    else{
+        list iterator = *l;
+        while(iterator->next != NULL)
+            iterator = iterator->next;
+        iterator->next  = element;
+    }
 }
 
 //print the list in parameter on the screen
 void display(list l){
-  if(l != NULL){
-    printf("[");
-    while(l != NULL){
-      printf("%d, ", l->value);
-      l = l->next;
+    if(l != NULL){
+        printf("[");
+        while(l != NULL){
+            printf("%d, ", l->value);
+            l = l->next;
+        }
+        printf("\b\b]\n");
     }
-    printf("\b\b]\n");
-  }
-  else{
-    printf("[]\n");
-  }
+    else{
+        printf("[]\n");
+    }
 }
 
 /*
@@ -50,77 +50,77 @@ void display(list l){
 
 //create a list of two integer
 list create_list_of_two_integer(){
-  int a;
-  list t = init();
-  list element1 = malloc(sizeof(struct cellule));
-  printf("Enter the first element: ");
-  scanf("%d", &a);
-  insert(&t, a);
-  printf("Enter the second element: ");
-  scanf("%d", &a);
-  insert(&t,a);
-  return t;
+    int a;
+    list t = init();
+    list element1 = malloc(sizeof(struct cellule));
+    printf("Enter the first element: ");
+    scanf("%d", &a);
+    insert(&t, a);
+    printf("Enter the second element: ");
+    scanf("%d", &a);
+    insert(&t,a);
+    return t;
 }
 
 
 //create a list of n integer
 list create_list_of_n_integer(int n){
-  list t = init();
-  int a;
-  for(int i=0; i < n; i++){
-    printf("Enter the new integer element: ");
-    scanf("%d", &a);
-    insert(&t, a);
-  }
-  return t;
+    list t = init();
+    int a;
+    for(int i=0; i < n; i++){
+        printf("Enter the new integer element: ");
+        scanf("%d", &a);
+        insert(&t, a);
+    }
+    return t;
 }
 
 //create a list of integer and insert him the integer while user don't write -1
 list create_list_integer(){
-  printf("[ Enter -1 if you want to stop the list ]\n"  );
-  list t = init();
-  int a;
-  do{
-      printf("Enter the new integer element: ");
-      scanf("%d", &a);
-      if(a != -1)
+    printf("[ Enter -1 if you want to stop the list ]\n"  );
+    list t = init();
+    int a;
+    do{
+        printf("Enter the new integer element: ");
+        scanf("%d", &a);
+        if(a != -1)
         insert(&t, a);
-  }while(a != -1);
-  return t;
+    }while(a != -1);
+    return t;
 }
 
 //search any element in the list
 void search_element(list l, int value){
-  list iterator = l;
-  while(iterator != NULL && iterator->value != value)
-    iterator = iterator->next;
+    list iterator = l;
+    while(iterator != NULL && iterator->value != value)
+        iterator = iterator->next;
 
-  if(iterator == NULL){
-    printf("Element [%d] not found.\n", value);
-  }
-  else{
-    printf("Element [%d] found.\n", value);
-  }
+    if(iterator == NULL){
+        printf("Element [%d] not found.\n", value);
+    }
+    else{
+        printf("Element [%d] found.\n", value);
+    }
 }
 
 //search the last occurence of any element in the list
 void search_last_occurence(list l, int value){
-  list iterator = l;
-  int pos = -1;
-  int i = 1;
-  while(iterator != NULL){
-    if(iterator->value == value)
-      pos = i;
-    iterator = iterator->next;
-    i++;
-  }
+    list iterator = l;
+    int pos = -1;
+    int i = 1;
+    while(iterator != NULL){
+        if(iterator->value == value)
+            pos = i;
+        iterator = iterator->next;
+        i++;
+    }
 
-  if( pos == -1){
-    printf("Element [%d] not found.\n", value);
-  }
-  else{
-    printf("Element [%d] found and the last occurence is at the position [%d].\n", value, pos);
-  }
+    if( pos == -1){
+        printf("Element [%d] not found.\n", value);
+    }
+    else{
+        printf("Element [%d] found and the last occurence is at the position [%d].\n", value, pos);
+    }
 
 }
 
@@ -128,62 +128,66 @@ void search_last_occurence(list l, int value){
   DELETE FUNCTION
 */
 void delete_first_element(list *l){
-  if(*l != NULL){
-    list element = *l;
-    *l = (*l)->next;
-    element->next = NULL;
-    free(element);
-  }
+    if(*l != NULL){
+        list element = *l;
+        *l = (*l)->next;
+        element->next = NULL;
+        free(element);
+    }
 }
 
 void delete_first_occurence(list *l, int value){
-  if(*l != NULL){
-    list element = *l;
-    if((*l)->value == value){
-      *l = (*l)->next;
+    if(*l != NULL){
+        list element = *l;
+        if((*l)->value == value){
+            *l = (*l)->next;
+            element->next = NULL;
+            free(element);
+        }
+        else{
+            list iterator = (*l)->next;
+            list prec = (*l);
+            while(iterator != NULL && iterator->value != value){
+                prec = iterator;
+                iterator = iterator->next;
+            }
+            if(iterator != NULL){
+                element = iterator;
+                prec->next = iterator->next;
+                element->next = NULL;
+                free(element);
+            }
+        }
     }
-    else{
-      list iterator = (*l)->next;
-      list prec = (*l);
-      while(iterator != NULL && iterator->value != value){
-        prec = iterator;
-        iterator = iterator->next;
-      }
-      element = iterator;
-      prec->next = iterator->next;
-    }
-    element->next = NULL;
-    free(element);
-  }
 }
 
 void delete_all_occurence(list *l, int value){
-  if(*l != NULL){
-    list iterator = *l;
-    list prec = *l;
-    while(iterator != NULL){
-      if(iterator->value == value){
-        if(iterator != *l){
-          list element = iterator;
-          prec->next = iterator->next;
-          element->next = NULL;
-          iterator = prec->next;
-          free(element);
+    if(*l != NULL){
+        list iterator = *l;
+        list prec = *l;
+        while(iterator != NULL){
+            if(iterator->value == value){
+                if(iterator != *l){
+                    list element = iterator;
+                    prec->next = iterator->next;
+                    element->next = NULL;
+                    iterator = prec->next;
+                    free(element);
+                }
+                else{
+                    list element = *l;
+                    *l = (*l)->next;
+                    element->next = NULL;
+                    free(element);
+                    iterator = *l;
+                    prec = *l;
+                }
+            }else{
+                prec = iterator;
+                iterator = iterator->next;
+            }
         }
-        else{
-          list element = *l;
-          *l = (*l)->next;
-          element->next = NULL;
-          free(element);
-          iterator = *l;
-          prec = *l;
-        }
-      }else{
-        prec = iterator;
-        iterator = iterator->next;
-      }
     }
-  }
 }
 
 
@@ -191,26 +195,26 @@ void delete_all_occurence(list *l, int value){
     double linked list
 */
 d_list create_d_list(){
-  printf("[ Enter -1 if you want to stop the list ]\n"  );
-  d_list l = NULL;
-  d_list last = l;
-  int a;
-  do{
+    printf("[ Enter -1 if you want to stop the list ]\n"  );
+    d_list l = NULL;
+    d_list last = l;
+    int a;
+    do{
     printf("Enter the value: ");
     scanf("%d", &a);
     if(a != -1){
-      d_list t = (d_list)malloc(sizeof(struct d_cellule));
-      t->next = NULL;
-      t->previous = NULL;
-      t->value = a;
-      if(l == NULL){
-        l = t;
-        last = l;
-      }else{
-        last->next = t;
-        t->previous = last;
-        last = t;
-      }
+        d_list t = (d_list)malloc(sizeof(struct d_cellule));
+        t->next = NULL;
+        t->previous = NULL;
+        t->value = a;
+        if(l == NULL){
+            l = t;
+            last = l;
+        }else{
+            last->next = t;
+            t->previous = last;
+            last = t;
+        }
     }
   }while(a != -1);
 
@@ -218,35 +222,35 @@ d_list create_d_list(){
 }
 
 void display_d_list_order_typing(d_list l){
-  if(l == NULL){
-    printf("[]\n");
-  }
-  else{
-    d_list iterator = l;
-    printf("[");
-    while(iterator != NULL){
-      printf("%d, ", iterator->value);
-      iterator = iterator->next;
+    if(l == NULL){
+        printf("[]\n");
     }
-    printf("\b\b]\n");
-  }
+    else{
+        d_list iterator = l;
+            printf("[");
+        while(iterator != NULL){
+            printf("%d, ", iterator->value);
+            iterator = iterator->next;
+        }
+        printf("\b\b]\n");
+    }
 
 }
 
 
 void display_d_list_inverse_order_typing(d_list l){
-  if(l == NULL){
-    printf("[]\n");
-  }else{
-    d_list iterator = l;
-    while(iterator->next != NULL)
-      iterator = iterator->next;
+    if(l == NULL){
+        printf("[]\n");
+    }else{
+        d_list iterator = l;
+        while(iterator->next != NULL)
+            iterator = iterator->next;
 
-    printf("[");
-    while(iterator != NULL){
-      printf("%d, ", iterator->value);
-      iterator = iterator->previous;
+        printf("[");
+        while(iterator != NULL){
+            printf("%d, ", iterator->value);
+            iterator = iterator->previous;
+        }
+        printf("\b\b]\n");
     }
-    printf("\b\b]\n");
-  }
 }
